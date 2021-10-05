622a8f18427221b8307a2ebed26c30183c67c1a6
sebastian.soderholm <sebastian.soderholm@fi.experis.com>
2021-10-05 13:41:05 +0300

    Added shipment Model class

---
+++ Models/Domain/Shipment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace boxinator.Models.Domain
+{
+    [Table("Shipment")]
+    public class Shipment
+    {
+        // Primary key
+        [Key]
+        public int Id { get; set; }
+
+        // Fields
+        [Required]
+        [MaxLength(100)]
+        public string RecieverName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string DestinationCountry { get; set; }
+
+        // Relationships
+        //public ICollection<Movie> Movies { get; set; }
+    }
+}
