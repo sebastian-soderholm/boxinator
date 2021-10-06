dadf20230dbcd139a4ebff653a4772c4a6653f72
sebastian.soderholm <sebastian.soderholm@fi.experis.com>
2021-10-06 15:28:05 +0300

    Added folder structure & login pages

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,10 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginPage } from './login/pages/login/login.page';
+import { RegisterPage } from './login/pages/register/register.page';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPage,
+    RegisterPage,
   ],
   imports: [
     BrowserModule,
