0592804e7bc14c82bfe151a50f4761593521324a
1l0v3m0un741n5 <>
2021-10-08 13:36:39 +0300

    added Create DTO for user

--- Models/DTO/User/UserCreateDTO.cs
+++ Models/DTO/User/UserCreateDTO.cs
@@ -1,30 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace boxinator.Models.Domain
+namespace boxinator.Models.DTO.User
 {
-    [Table("User")]
-    public class User
+    public class UserCreateDTO
     {
-        [Key]
-        public int Id { get; set; }
-        [MaxLength(50)]
         public string FirstName { get; set; }
-        [MaxLength(100)]
         public string LastName { get; set; }
-        [Required]
-        [MaxLength(100)]
         public string Email { get; set; }
         public int CountryId { get; set; }
-        public Country Country { get; set; }
         public string ZipCode { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string PhoneNumber { get; set; }
-        [Required]
-        public string AccountType { get; set; }
     }
 }
