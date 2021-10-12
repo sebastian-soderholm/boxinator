1e5164a8bb5d3d1812364ab07285cd1f5a441494
sebastian.soderholm <sebastian.soderholm@fi.experis.com>
2021-10-12 23:37:28 +0300

    added cost to shipment models

--- ClientApp/src/app/shipment/models/guest-shipment.model.ts
+++ ClientApp/src/app/shipment/models/guest-shipment.model.ts
@@ -4,6 +4,7 @@ import { BasicBox, HumbleBox, DeluxeBox, PremiumBox } from "./box.model";
 export interface GuestShipment {
   guestEmail: string;
   receiverName: string;
+  cost: number;
   boxes: [BasicBox | HumbleBox | DeluxeBox | PremiumBox];
   destinationCountryId: number;
 }
