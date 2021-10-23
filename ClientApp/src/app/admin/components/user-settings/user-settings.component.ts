7aa6e6e23a10755cc761b0f6d6b1cf08a5601a48
1l0v3m0un741n5 <>
2021-10-23 08:23:05 +0300

    added form validation to user search, updated validation for edit shipments

--- ClientApp/src/app/admin/components/user-settings/user-settings.component.ts
+++ ClientApp/src/app/admin/components/user-settings/user-settings.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, createPlatform, OnInit } from '@angular/core';
 import { AccountService } from 'src/app/account/services/account.service';
 import { SessionService } from 'src/app/shared/session.service';
 import { User } from '../../../account/models/user.model';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings',
@@ -10,18 +10,26 @@ import { User } from '../../../account/models/user.model';
   styleUrls: ['./user-settings.component.scss']
 })
 export class UserSettingsComponent implements OnInit {
-  searchvalue: string = "";
+  //searchvalue: string = "";
   loadComponent: boolean = false;
   loadList: boolean = false;
   users: User[] | undefined;
   selectedUser: User | undefined;
+  searchForm: FormGroup | any;
+  searchvalue = new FormControl('', [Validators.required]);
 
   constructor(
     private readonly _accountService: AccountService,
     private readonly _sessionService: SessionService,
-  ) { }
+  ) {}
 
   ngOnInit(): void {
+    this.searchForm = new FormGroup({
+      searchvalue: new FormControl('', [
+        Validators.required,
+        Validators.pattern("[a-zA-Z0-9-ZÆæØøßÅÄÖåäö]*"),
+      ])
+    });
   }
 
   selectUser(user: User) {
@@ -30,15 +38,17 @@ export class UserSettingsComponent implements OnInit {
     this._sessionService.setFetchedUserInfo(user)
   }
 
-  public onSearch(input: string): void {
+  public onSearch(): void {
+    const searchInput = this.searchForm.get('searchvalue').value;
     this.loadComponent = false;
-    if(Number(input)) {
-      this._accountService.getUserById(Number(input), async () => {
+    
+    if(Number(searchInput)) {
+      this._accountService.getUserById(Number(searchInput), async () => {
         await console.log("success"), this.loadComponent = true;
       });
     }
     else {
-      this._accountService.getBySearchTerm(input, async () => {
+      this._accountService.getBySearchTerm(String(searchInput), async () => {
         await console.log("success"),
         this.users = this._sessionService.usersForAdmin;
         this.loadList = true//, this.loadComponent = true;
