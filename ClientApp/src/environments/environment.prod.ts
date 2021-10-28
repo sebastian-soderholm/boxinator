83a5c0cebd3b4e416d55af0c742669b136c9309c
sebastian.soderholm <sebastian.soderholm@fi.experis.com>
2021-10-28 10:26:49 +0300

    added empty properties

---
+++ ClientApp/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  },
+  baseURL: ''
+};
